b8ee070f9607b13d2add72c9254eccb3cbf715a6
nitesh026 <103298078+nitesh026@users.noreply.github.com>
2025-04-13 05:03:08 +0530

    Update AddWidget.js

--- src/components/AddWidget.js
+++ src/components/AddWidget.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import '../css/Widget.css'
 
-const addWidgets = () => {
-    
-}
-
-
 const AddWidget = () => {
+  const [data,setData] = useState(null);
+  const [loading,setLoading] = useState(true);
+  const [error,setError] = useState(null);
+  
+  const postData = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+        const response = await fetch("http://localhost:8080/v1/createWidget",{
+          method:"POST",
+          headers:
+          { "Content-Type":"Application/json"},
+          body: JSON.stringify({
+            "name":"Laptop",
+            "description":"This is HP Laptop",
+            "price": "12000"
+          }),
+  });
+  if(!response.ok){
+    throw new Error("Failed to post data");
+  }
+  const result = await response.json();
+      setData(result);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div>
         <label className='label'>
